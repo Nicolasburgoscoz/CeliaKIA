d77aae862eb1e32afb203f109c7c5e367896be5d
Nicolas <nico.burgoscoz@gmail.com>
2020-08-01 16:18:21 -0300

    Initial commit

---
+++ app/src/main/java/com/ids/celiakia/HomeActivity.kt
@@ -0,0 +1,104 @@
+package com.ids.celiakia
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Menu
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.content_main.*
+import java.security.Provider
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE,
+    FACEBOOK
+}
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+        //Setup
+
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val provider =bundle?.getString("provider")
+        setup(email ?:"", provider ?: "")
+
+        // Guardado de datos
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email",email)
+        prefs.putString("provider",provider)
+        prefs.apply()
+    }
+
+    private fun setup(email: String, provider: String) {
+
+        title = "Inicio"
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+
+           // Borrado de datos
+
+            val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            if (provider == ProviderType.FACEBOOK.name) {
+                LoginManager.getInstance().logOut()
+            }
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.home, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+}
